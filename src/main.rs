d1a0d3fd9d1ff635b0d2bd726fea43ac7d35c14a
Nate Catelli <natec@cakesolutions.net>
2018-07-21 20:24:47 -0400

    Initial commit.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+extern crate ws;
+
+fn main () {
+    if let Err(error) = ws::listen("127.0.0.1:3012", |out| {
+        move |msg| {
+            println!("Server got message '{}'. ", msg);
+            out.send(msg)
+        }
+
+    }) {
+        println!("Failed to create WebSocket due to {:?}", error);
+    }
+
 }
