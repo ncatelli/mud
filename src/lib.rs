9fade72873f0bd947dc2b6edbdee5b3026d9349c
Nate Catelli <natec@cakesolutions.net>
2018-08-15 11:59:40 -0400

    Move web to it's own submodule

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,7 @@ mod web;
 pub fn run() -> Result<(), &'static str> {
     env_logger::init();
 
-    match ws::listen("0.0.0.0:3012", |out| {
-        // Use our router as the handler to route the new connection
-        web::Router::new(out)
-    }) {
+    match web::listen("0.0.0.0:3012") {
         Ok(_) => Ok(()),
         Err(_) => Err("Unable to bind to address."),
     }
--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,3 +1,13 @@
+extern crate ws;
+
 mod router;
 
-pub use self::router::Router;
+pub fn listen(addr: &str) -> Result<(), &'static str> {
+    match ws::listen(&addr, |out| {
+        // Use our router as the handler to route the new connection
+        router::Router::new(out)
+    }) {
+        Ok(_) => Ok(()),
+        Err(_) => Err("Unable to bind to address."),
+    }
+}
