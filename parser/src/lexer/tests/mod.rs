0c752895d3b2bf5f192b6458daf9a8da069a1647
Nate Catelli <natec@cakesolutions.net>
2018-08-19 10:56:55 -0400

    Add additional type tests.

--- parser/src/lexer/tests/mod.rs
+++ parser/src/lexer/tests/mod.rs
@@ -2,12 +2,35 @@ use super::*;
 
 #[test]
 fn can_lex_integers() {
-    let lexeme = "5";
-    assert_eq!(Lexeme::Integer(5), lex(lexeme).unwrap()[0])
+    assert_eq!(Lexeme::Integer(5), lex("5").unwrap()[0])
 }
 
 #[test]
 fn can_lex_chars() {
-    let lexeme = "c";
-    assert_eq!(Lexeme::Char('c'), lex(lexeme).unwrap()[0])
+    assert_eq!(Lexeme::Char('c'), lex("c").unwrap()[0])
+}
+
+#[test]
+fn can_lex_left_paren() {
+    assert_eq!(Lexeme::LeftParen, lex("(").unwrap()[0])
+}
+
+#[test]
+fn can_lex_right_paren() {
+    assert_eq!(Lexeme::RightParen, lex(")").unwrap()[0])
+}
+
+#[test]
+fn can_lex_colon() {
+    assert_eq!(Lexeme::Colon, lex(":").unwrap()[0])
+}
+
+#[test]
+fn can_lex_semicolon() {
+    assert_eq!(Lexeme::Semicolon, lex(";").unwrap()[0])
+}
+
+#[test]
+fn can_lex_period() {
+    assert_eq!(Lexeme::Period, lex(".").unwrap()[0])
 }
