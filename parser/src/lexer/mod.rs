67aba3c1a6cf703589c842d8427fd086acaba2fc
Nate Catelli <natec@cakesolutions.net>
2018-08-19 10:47:58 -0400

    Simple lexer.

--- parser/src/lexer/mod.rs
+++ parser/src/lexer/mod.rs
@@ -1,17 +1,73 @@
-pub enum Token {
+#[derive(Debug)]
+enum Token<'a> {
+    Operator(&'a Operator),
+    Operand(&'a ComplexType<'a>),
     Call,
     Args,
-    Operator(char),
-    Operand(ComplexType),
 }
 
-enum ComplexType {
-    Array(Vec<Primitive>),
-    Primitive(Primitive),
+#[derive(Debug)]
+enum ComplexType<'a> {
+    Array(Vec<&'a Primitive<'a>>),
+    Primitive(&'a Primitive<'a>),
+    ObjectId(&'a str),
 }
 
-enum Primitive {
+#[derive(Debug)]
+enum Operator {
+    Op(char),
+}
+
+#[derive(Debug)]
+enum Primitive<'a> {
     Float(f64),
     Int(i64),
-    Str(&str),
+    Str(&'a str),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Lexeme {
+    Char(char),
+    Integer(u32),
+    Period,
+    Semicolon,
+    Colon,
+    LeftParen,
+    RightParen,
+    Error(char),
+}
+
+/// Lexes each character into an array of Lexemes.
+fn lex(input: &str) -> Result<Vec<Lexeme>, &'static str> {
+    let lex_vec = input.chars().map(|c| {
+        match c {
+            c if c.is_alphabetic() => Lexeme::Char(c),
+            c if c.is_digit(10) => Lexeme::Integer(c.to_digit(10).unwrap()),
+            '.' => Lexeme::Period,
+            ':' => Lexeme::Colon,
+            ';' => Lexeme::Semicolon,
+            '(' => Lexeme::LeftParen,
+            ')' => Lexeme::RightParen,
+            _   => Lexeme::Error(c),
+        }
+    }).collect();
+
+    Ok(lex_vec)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_lex_integers() {
+        let lexeme = "5";
+        assert_eq!(Lexeme::Integer(5), lex(lexeme).unwrap()[0])
+    }
+
+    #[test]
+    fn can_lex_chars() {
+        let lexeme = "c";
+        assert_eq!(Lexeme::Char('c'), lex(lexeme).unwrap()[0])
+    }
 }
--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,3 +1,5 @@
+mod lexer;
+
 //pub fn parse<'a>(stmt: &'a str) -> Result<&'a Statement, &'static str> {
 //    Err("Invalid Parse")
 //}
