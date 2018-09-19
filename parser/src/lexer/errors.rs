07d73963f64530587452398fdb853f52fc1ae2a0
Nate Catelli <natec@cakesolutions.net>
2018-09-19 13:58:52 -0400

    Add new

--- parser/src/lexer/errors.rs
+++ parser/src/lexer/errors.rs
@@ -11,6 +11,16 @@ struct ParseError {
     Column: u32
 }
 
+impl ParseError {
+    fn new(ek: ParseError, l: String, c: u32) -> ParseError {
+        ParseError {
+            ErrorKind: ek,
+            Line: l,
+            Column: c
+        }
+    }
+}
+
 impl fmt::Display for ParseError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Shit broke")
